4d4fd8b425bfa46159bc42511b83666cb8920d30
Adam Veldhousen <adamveld12@users.noreply.github.com>
2020-04-06 13:30:24 -0500

    added ability to set user agent via args in test cafe CLI (#1)

--- src/index.js
+++ src/index.js
@@ -1,71 +1,96 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 export default {
     // Multiple browsers support
     isMultiBrowser: true,
 
     browser: null,
+    userAgent: null,
 
     openedPages: {},
 
     // Required - must be implemented
     // Browser control
-    async openBrowser (id, pageUrl, browserName) {
-        const browserArgs = browserName.split(':');
+    async openBrowser(id, pageUrl, browserName) {
+        const browserArgs = browserName.split(":");
         if (!this.browser) {
             const launchArgs = {
-                timeout: 10000
+                timeout: 10000,
             };
 
-            const noSandboxArgs = ['--no-sandbox', '--disable-setuid-sandbox'];
+            const noSandboxArgs = ["--no-sandbox", "--disable-setuid-sandbox"];
 
-            if (browserArgs.indexOf('no_sandbox') !== -1) launchArgs.args = noSandboxArgs;
-            else if (browserName.indexOf('?') !== -1) {
-                const userArgs = browserName.split('?');
+            if (browserArgs.indexOf("no_sandbox") !== -1)
+                launchArgs.args = noSandboxArgs;
+            else if (browserName.indexOf("?") !== -1) {
+                const userArgs = browserName.split("?");
                 const params = userArgs[0];
 
-                if (params === 'no_sandbox') launchArgs.args = noSandboxArgs;
+                if (params === "no_sandbox") launchArgs.args = noSandboxArgs;
 
                 const executablePath = userArgs[1];
 
                 if (executablePath.length > 0)
                     launchArgs.executablePath = executablePath;
             }
+
+            this.userAgent = parseUserAgentFromArgs(browserArgs);
+
             this.browser = await puppeteer.launch(launchArgs);
         }
 
         const page = await this.browser.newPage();
 
-        const emulationArg = browserArgs.find(v => /^emulate/.test(v));
+        if (this.userAgent) {
+            console.log(`USING USERAGENT: ${this.userAgent}`);
+            page.setUserAgent(this.userAgent);
+        }
+
+        const emulationArg = browserArgs.find((v) => /^emulate/.test(v));
 
         if (Boolean(emulationArg)) {
-          const [, emulationDevice] = emulationArg.split('=');
-          const device = puppeteer.devices[emulationDevice];
+            const [, emulationDevice] = emulationArg.split("=");
+            const device = puppeteer.devices[emulationDevice];
 
-          if (!device) throw new Error('Emulation device is not supported'); 
+            if (!device) throw new Error("Emulation device is not supported");
 
-          await page.emulate(device);
+            await page.emulate(device);
         }
 
         await page.goto(pageUrl);
         this.openedPages[id] = page;
     },
 
-    async closeBrowser (id) {
+    async closeBrowser(id) {
         delete this.openedPages[id];
         await this.browser.close();
     },
 
-    async isValidBrowserName () {
+    async isValidBrowserName() {
         return true;
     },
 
     // Extra methods
-    async resizeWindow (id, width, height) {
+    async resizeWindow(id, width, height) {
         await this.openedPages[id].setViewport({ width, height });
     },
 
-    async takeScreenshot (id, screenshotPath) {
+    async takeScreenshot(id, screenshotPath) {
         await this.openedPages[id].screenshot({ path: screenshotPath });
-    }
+    },
 };
+
+function parseUserAgentFromArgs(browserArgs = []) {
+    const uaIdx = browserArgs.findIndex((x) => x.includes("user_agent="));
+    if (uaIdx !== -1) {
+        const matches = browserArgs[uaIdx].match(
+            /user_agent=[\"\']?(.*)[\"\']?/i
+        );
+
+        if (matches.length > 1) {
+            return matches[1];
+        }
+    }
+
+    return null;
+}
